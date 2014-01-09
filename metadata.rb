9203994236190ad782f394169ab626680a20681c
Vladimir Yarotsky <v.yarotsky@itransition.com>
2014-01-09 22:14:58 +0300

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'unicorn'
+maintainer       'Vladimir Yarotsky'
+maintainer_email 'vladimir.yarotsky@gmail.com'
+license          'MIT'
+description      'Installs/Configures unicorn and nginx vhosts'
+version          '0.0.1'
+
+depends "nginx"
